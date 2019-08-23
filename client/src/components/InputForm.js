94a21344848594c90cc9b3e31719304056f1eb50
Clint Brodar <clintbrodar@gmail.com>
2019-08-22 23:48:57 -0700

    Task card

---
+++ client/src/components/InputForm.js
@@ -0,0 +1,162 @@
+import React from 'react';
+import clsx from 'clsx';
+import { makeStyles } from '@material-ui/core/styles';
+import MenuItem from '@material-ui/core/MenuItem';
+import TextField from '@material-ui/core/TextField';
+
+const currencies = [
+  {
+    value: 'USD',
+    label: '$',
+  },
+  {
+    value: 'EUR',
+    label: '€',
+  },
+  {
+    value: 'BTC',
+    label: '฿',
+  },
+  {
+    value: 'JPY',
+    label: '¥',
+  },
+];
+
+const useStyles = makeStyles(theme => ({
+  container: {
+    display: 'flex',
+    flexWrap: 'wrap',
+  },
+  textField: {
+    marginLeft: theme.spacing(1),
+    marginRight: theme.spacing(1),
+  },
+  dense: {
+    marginTop: theme.spacing(2),
+  },
+  menu: {
+    width: 200,
+  },
+}));
+
+export default function OutlinedTextFields() {
+  const classes = useStyles();
+  const [values, setValues] = React.useState({
+    name: 'Cat in the Hat',
+    age: '',
+    multiline: 'Controlled',
+    currency: 'USD',
+  });
+
+  const handleChange = name => event => {
+    setValues({ ...values, [name]: event.target.value });
+  };
+
+  return (
+    <form className={classes.container} noValidate autoComplete="off">
+      <TextField
+        required
+        id="outlined-required"
+        label="Client Name"
+        placeholder="Client Name"
+        className={classes.textField}
+        value={values.name}
+        onChange={handleChange('name')}
+        margin="normal"
+        variant="outlined"
+      />
+      <TextField
+        required
+        id="outlined-required"
+        label="Sales Agent"
+        placeholder="Assigned Agent Name"
+        className={classes.textField}
+        value={values.name}
+        onChange={handleChange('name')}
+        margin="normal"
+        variant="outlined"
+      />
+      <TextField
+        id="outlined-email-input"
+        label="Email"
+        className={classes.textField}
+        type="email"
+        name="email"
+        autoComplete="email"
+        margin="normal"
+        variant="outlined"
+      />
+      <TextField
+        id="outlined-number"
+        label="Phone Number"
+        value={values.age}
+        onChange={handleChange('age')}
+        type="number"
+        className={classes.textField}
+        InputLabelProps={{
+          shrink: true,
+        }}
+        margin="normal"
+        variant="outlined"
+      />
+      <TextField
+        id="outlined-multiline-static"
+        label="Description"
+        multiline
+        rows="4"
+        defaultValue="User would input description of client business"
+        className={classes.textField}
+        margin="normal"
+        variant="outlined"
+      />
+      <TextField
+        id="outlined-select-currency"
+        select
+        label="Select"
+        className={classes.textField}
+        value={values.currency}
+        onChange={handleChange('currency')}
+        SelectProps={{
+          MenuProps: {
+            className: classes.menu,
+          },
+        }}
+        helperText="Please select your currency"
+        margin="normal"
+        variant="outlined"
+      >
+        {currencies.map(option => (
+          <MenuItem key={option.value} value={option.value}>
+            {option.label}
+          </MenuItem>
+        ))}
+      </TextField>
+      <TextField
+        id="outlined-number"
+        label="Potential Value"
+        value={values.age}
+        onChange={handleChange('age')}
+        type="number"
+        className={classes.textField}
+        InputLabelProps={{
+          shrink: true,
+        }}
+        margin="normal"
+        variant="outlined"
+      />
+      <TextField
+        id="outlined-full-width"
+        label="Kind of business"
+        style={{ margin: 8 }}
+        placeholder="What kind of business is this?"
+        fullWidth
+        margin="normal"
+        variant="outlined"
+        InputLabelProps={{
+          shrink: true,
+        }}
+      />
+    </form>
+  );
+}
