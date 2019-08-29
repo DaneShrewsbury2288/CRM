e3c538dded651b5fd9c8288f4a0d7f1263cc9f34
Clint Brodar <clintbrodar@gmail.com>
2019-08-28 18:13:46 -0700

    Client populates orders and users

--- controllers/clientController.js
+++ controllers/clientController.js
@@ -4,12 +4,25 @@ const Client = db.Client;
 module.exports = {
   findAll: function(req, res) {
     Client
-      .find(req.query)
-      .sort({ date: -1 })
-      .populate("orders")
-      .then(dbModel => res.json(dbModel))
-      .catch(err => res.status(422).json(err));
-  },
+    .find(req.query)
+    .sort({ date: -1 })
+    // populate orders
+    .populate("order", "_id fulfilled created_at")
+    // populate associated user
+    .populate("user", "_id firstName lastName")
+    .then(dbModel => {
+      res.status(200).json({
+        orders: dbModel.map(model => {
+          return {
+            _id: model._id,
+            product: model.order,
+            user: model.user
+          };
+        })
+      })
+    })
+    .catch(err => res.status(422).json(err));
+},
   findById: function(req, res) {
     Client
       .findById(req.params.id)
@@ -20,6 +33,14 @@ module.exports = {
   create: function(req, res) {
     Client
       .create(req.query)
+      // associate all orders with client
+      .then(function (dbOrder) {
+        return db.Order.findOneAndUpdate({}, { $push: { products: dbOrder._id } }, { new: true });
+      })
+      // associate user ID with order
+      .then(function (dbUser) {
+        return db.User.findOneAndUpdate({}, { $push: { users: dbUser._id } }, { new: true });
+      })
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
