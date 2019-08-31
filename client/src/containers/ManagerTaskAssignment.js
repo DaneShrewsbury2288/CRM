02e52c99da2d8d8b8b4a84c696bd805e477f037f
Clint Brodar <clintbrodar@gmail.com>
2019-08-30 20:24:02 -0700

    Clients and users console logged on click

--- client/src/containers/ManagerTaskAssignment.js
+++ client/src/containers/ManagerTaskAssignment.js
@@ -3,6 +3,7 @@ import React, { Component } from "react";
 // import Card from '../components/Card';
 // import InputForm from '../components/InputForm';
 import API from '../utilities/api';
+import UserAPI from '../utils/API';
 
 class ManagerTaskAssignment extends Component {
     state = {
@@ -12,17 +13,36 @@ class ManagerTaskAssignment extends Component {
     }
     UNSAFE_componentWillMount() {
         this.checkTasks();
+        this.checkClients();
+        this.checkUsers();
     }
     // get tasks
     checkTasks = () => {
         API.getTasks()
             .then(res =>
-                this.setState({ tasks: res.data.tasks})
+                this.setState({ tasks: res.data.tasks })
                 )
                 .catch(error => console.log("Check task error: " + error))
     }
+    // get clients
+    checkClients = () => {
+        API.getClients()
+            .then(res => 
+                this.setState({ clients: res.data.clients })
+                )
+                .catch(error => console.log("Check task error: " + error))
+    }
+    // get users
+    checkUsers = () => {
+        UserAPI.getUsers()
+            .then(res =>
+                this.setState({ users: res.data })
+                )
+    }
     checkState = () => {
         console.log(this.state.tasks);
+        console.log(this.state.clients);
+        console.log(this.state.users);
     }
         // assigned status
         // completion status
@@ -32,7 +52,6 @@ class ManagerTaskAssignment extends Component {
         // get user associated with each task
             // get each individual ids
             // get clients associated with each task
-    // get clients
     // get users
 
     render() {
