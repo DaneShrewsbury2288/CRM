eb5fede4c3df74f1d0bb7ee1a1c6f25113ab8f2f
bottomshorter <lamvanthony@gmail.com>
2019-08-31 14:13:00 -0700

    moved Kanban to Sales Team Daily

--- client/src/containers/NewsFeed.js
+++ client/src/containers/NewsFeed.js
@@ -1,31 +1,11 @@
-import React, { Component } from "react";
+import React from "react";
 import PageTitle from "../components/PageTitle";
-import Kanban from "../components/Kanban";
-import { withStyles } from '@material-ui/core/styles';
-import clsx from 'clsx';
 
-const styles = theme => ({
-  root: {
-      display: 'flex',
-  },
-  kanban: {
-    height: 500
-  }
-});
-
-
-class NewsFeed extends Component {
-  render(){
-    const { classes } = this.props;
-    const KanbanHeight = clsx(classes.kanban);
-
+export default function NewsFeed() {
     return (
         <div>
         <PageTitle title="News Feed" />
-        <Kanban className={KanbanHeight} />
        </div>
     );
-    }
   }
 
-export default withStyles(styles)(NewsFeed);
