0e17bc03dfe1a8270a5db698627d21cfc89b71fc
bottomshorter <lamvanthony@gmail.com>
2019-09-03 01:19:50 -0700

    passed through array to Review Page

--- client/src/components/EditableTable.js
+++ client/src/components/EditableTable.js
@@ -300,6 +300,8 @@ export default function EnhancedTable() {
 
   const emptyRows = rowsPerPage - Math.min(rowsPerPage, rows.length - page * rowsPerPage);
 
+  console.log(selected);
+
   return (
     <div className={classes.root}>
       <Paper className={classes.paper}>
--- client/src/components/Review.js
+++ client/src/components/Review.js
@@ -5,14 +5,16 @@ import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
 import Grid from '@material-ui/core/Grid';
+import selected from '../components/EditableTable';
 
 const products = [
   { name: 'Product 1', desc: 'A nice thing', price: '$9.99' },
   { name: 'Product 2', desc: 'Another thing', price: '$3.45' },
   { name: 'Product 3', desc: 'Something else', price: '$6.51' },
   { name: 'Product 4', desc: 'Best thing of all', price: '$14.11' },
-  { name: 'Shipping', desc: '', price: 'Free' },
 ];
+
+
 const addresses = ['1 Material-UI Drive', 'Reactville', 'Anytown', '99999', 'USA'];
 const payments = [
   { name: 'Card type', detail: 'Visa' },
@@ -35,6 +37,8 @@ const useStyles = makeStyles(theme => ({
 
 export default function Review() {
   const classes = useStyles();
+
+  console.log(selected);
 
   return (
     <React.Fragment>
