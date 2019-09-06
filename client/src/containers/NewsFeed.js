31469120abc8bf3e451a7222a83d3e7e37e6fba7
bottomshorter <lamvanthony@gmail.com>
2019-09-06 12:39:07 -0700

    added ProuctBreakdown

--- client/src/containers/NewsFeed.js
+++ client/src/containers/NewsFeed.js
@@ -1,12 +1,14 @@
 import React from "react";
 import PageTitle from "../components/PageTitle";
 import Example from "../components/ProductChart";
+import SalesBreakdown from "../components/SalesBreakdown";
 
 export default function NewsFeed() {
     return (
         <div>
         <PageTitle title="News Feed" />
         <Example />
+        <SalesBreakdown />
        </div>
     );
   }
