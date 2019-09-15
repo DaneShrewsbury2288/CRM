6b5940afd9c9ec095d27fd45ced5c279041f7cd0
jsevern94 <severnj@spu.edu>
2019-09-14 23:59:34 -0700

    revamped messaging system to keep the input and back buttons in fixed positions and changed styling a bit for a more user friendly feel.

--- client/src/components/Messenger.js
+++ client/src/components/Messenger.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import API from "../utils/API";
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
+import Button from '@material-ui/core/Button';
 import Dialogue from "./Dialogue";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
@@ -63,48 +64,70 @@ class Messenger extends Component {
         overflowY: 'scroll',
         overflow: 'auto'
       },
+      header: {
+        position: 'fixed',
+        marginTop: 0,
+        zIndex: 10,
+      },
       title: {
         backgroundColor: '#3f51b5',
         width: '300px',
-        position: 'fixed',
-        marginTop: 0,
+        margin: 0,
         padding: 8,
         color: 'white',
         zIndex: 10,
         borderTopRightRadius: '.5rem',
         borderTopLeftRadius: '.5rem'
       },
+      back: {
+        margin: '8px',
+        display: 'flex',
+        justifyContent: 'flex-start'
+      },
       content: {
-        marginTop: '55px'
+        marginTop: '38px',
+        minHeight: '250px'
+      },
+      space: {
+        padding: '8px'
       }
     }
 
     return (
       <div style={styles.body}>
-        <h4 onClick={this.hideMessenger} style={styles.title}>Messenger</h4>
-      {this.state.display ?
-        <div style={styles.content}>
-          {this.state.partner
-            ?
-            <div>
-              <h4 onClick={this.back}>Back</h4>
-              <Dialogue user={user._id} partner={this.state.partner} />
+        <div style={styles.header}>
+          <h1 onClick={this.hideMessenger} style={styles.title}>Messenger</h1>
+          {this.state.partner ?
+          <div style={styles.back}>
+            <Button variant="contained" color="primary"onClick={this.back}>Back</Button>
             </div>
             :
-            <List>
-              {this.state.users.map(user => (
-                <ListItem key={user._id}>
-                  <h6 id={user._id} onClick={this.handleSetPartner}>
-                    {user.firstName} {user.lastName}
-                  </h6>
-                </ListItem>
-              ))}
-            </List>
+            null
           }
         </div>
-        :
-        <h2> </h2>
-      }
+        {this.state.display ?
+          <div style={styles.content}>
+            {this.state.partner
+              ?
+              <div>
+
+                <Dialogue user={user._id} partner={this.state.partner} />
+              </div>
+              :
+              <List>
+                {this.state.users.map(user => (
+                  <ListItem key={user._id}>
+                    <h2 id={user._id} onClick={this.handleSetPartner}>
+                      {user.firstName} {user.lastName}
+                    </h2>
+                  </ListItem>
+                ))}
+              </List>
+            }
+          </div>
+          :
+          <h1 style={styles.space}> </h1>
+        }
       </div>
     )
   }
