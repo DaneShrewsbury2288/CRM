b4fd3036647e44cfa1bb43817c64f3d006397e37
Robert <46203504+rdweston1995@users.noreply.github.com>
2019-09-16 13:18:12 -0700

    Have markers appearing now trying to get it to work through address

--- client/src/components/DiscoverMap.js
+++ client/src/components/DiscoverMap.js
@@ -6,8 +6,33 @@ import {
   Marker,
   // InfoWindow
 } from "react-google-maps";
+import Geocode from "react-geocode";
+
+getLatLng = () => {
+  Geocode.fromAddress("Von's 1000SPIRITS").then(
+    response => {
+      const { lat, lng } = response.results[0].geometry.location;
+      console.log(lat, lng);
+    },
+    error => {
+      console.log(error);
+    }
+  )
+}
 
 function Map() {
+  // getLatLan = () => {
+  //   Geocode.fromAddress("Von's 1000SPIRITS").then(
+  //     response => {
+  //       const { lat, lng } = response.results[0].gometry.location;
+  //       console.log(lat, lng);
+  //     },
+  //       error => {
+  //         console.log(error);
+  //       }
+  //   )
+  // };
+
   return (
     // <GoogleMap
     //   defaultZoom={10}
@@ -17,8 +42,11 @@ function Map() {
       defaultZoom={10}
       defaultCenter={{ lat: 47.6062, lng: -122.3321 }}
     >
-      <Marker
+      {/* <Marker
         position={{lat: 47.606564, lng: -122.338312 }}
+        /> */}
+        <Marker
+          position={{}}
         />
     </GoogleMap>
   )
