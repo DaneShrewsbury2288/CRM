52b93d4a5e6732786c224a8804340ab46ebf119f
bottomshorter <lamvanthony@gmail.com>
2019-09-16 21:53:31 -0700

    PASSING QUANTITY THRU STATE FINALLY

--- client/src/components/Review.js
+++ client/src/components/Review.js
@@ -39,6 +39,7 @@ export default function Review() {
     ExtraFoamLimitedQuantity: 0,
     TheKrakenQuantity: 0,
     SamsBeerQuantity: 0
+    
 
   }]);
   // const [values, setValues] = React.useState({
@@ -49,6 +50,7 @@ export default function Review() {
   // });
 
   const classes = useStyles();
+
 
 
   const x = Table.selectedBrews.arrayOne.slice(-1);
@@ -66,7 +68,13 @@ export default function Review() {
 
   function checkState() {
     console.log(Table.rows);
+    console.log(values);
   }
+ 
+
+  // function checkStateAll(){
+  //   console.log(BlackRavenQuantity);
+  // }
 
   // seeObject();
 
@@ -77,19 +85,20 @@ export default function Review() {
       </Typography>
       <button onClick={checkState}>Click me to check state</button>
       <List>
+      {/* <button onClick={checkStateAll}>Click me to check state</button> */}
           <ListItem
             className={classes.listItem}
-            key="0">
+            key="1">
             <ListItemText
               primary="Black Raven Trickster" />
             <Typography
               variant="body2">$5</Typography>
             <Typography>
               <TextField
-                id="0"
+                id="1"
                 name="We"
                 label="Quantity Desired"
-                value={values}
+                value={values.quantity}
                 onChange={handleChange('BlackRavenQuantity')}
                 type="number"
                 className={classes.textField}
@@ -102,14 +111,14 @@ export default function Review() {
           </ListItem>
           <ListItem
             className={classes.listItem}
-            key="1">
+            key="2">
             <ListItemText
               primary="Hops Potato" />
             <Typography
               variant="body2">$7.5</Typography>
             <Typography>
               <TextField
-                id="1"
+                id="2"
                 name="you"
                 label="Quantity Desired"
                 value={values.quantity}
@@ -125,14 +134,14 @@ export default function Review() {
           </ListItem>
           <ListItem
             className={classes.listItem}
-            key="2">
+            key="3">
             <ListItemText
               primary="Sizzlebird Cider" />
             <Typography
               variant="body2">$8</Typography>
             <Typography>
               <TextField
-                id="1"
+                id="3"
                 name="you"
                 label="Quantity Desired"
                 value={values.quantity}
@@ -148,14 +157,14 @@ export default function Review() {
           </ListItem>
           <ListItem
             className={classes.listItem}
-            key="3">
+            key="4">
             <ListItemText
               primary="Sounds Puget" />
             <Typography
               variant="body2">$8.89</Typography>
             <Typography>
               <TextField
-                id="1"
+                id="4"
                 name="you"
                 label="Quantity Desired"
                 value={values.quantity}
@@ -171,14 +180,14 @@ export default function Review() {
           </ListItem>
           <ListItem
             className={classes.listItem}
-            key="4">
+            key="5">
             <ListItemText
               primary="Extra Foam - Limited Edition" />
             <Typography
               variant="body2">$9.19</Typography>
             <Typography>
               <TextField
-                id="1"
+                id="5"
                 name="you"
                 label="Quantity Desired"
                 value={values.quantity}
@@ -194,14 +203,14 @@ export default function Review() {
           </ListItem>
           <ListItem
             className={classes.listItem}
-            key="5">
+            key="6">
             <ListItemText
               primary="The Kraken" />
             <Typography
               variant="body2">$9.49</Typography>
             <Typography>
               <TextField
-                id="1"
+                id="6"
                 name="you"
                 label="Quantity Desired"
                 value={values.quantity}
@@ -217,14 +226,14 @@ export default function Review() {
           </ListItem>
           <ListItem
             className={classes.listItem}
-            key="6">
+            key="7">
             <ListItemText
               primary="Sam's Beer" />
             <Typography
               variant="body2">$14.99</Typography>
             <Typography>
               <TextField
-                id="1"
+                id="7"
                 name="you"
                 label="Quantity Desired"
                 value={values.quantity}
